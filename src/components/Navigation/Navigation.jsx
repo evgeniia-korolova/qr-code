def942673fa19901ab27ab999a373725addb6b3e
evgeniia-korolova <jkod@ukr.net>
2024-09-10 02:00:51 +0300

    fix paths

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,10 +4,10 @@ import s from './Navigation.module.css'
 const Navigation = () => {
   return (
     <nav className={s.container}>
-        <Link to="/generate">Generate QR Code</Link>
-        <Link to="/scan">Scan QR Code</Link>
-        <Link to="/generateHistory">History of Generated QR Codes</Link>
-        <Link to="/scanHistory">History of Scanned QR Codes</Link>
+        <Link to="/qr-code/generate">Generate QR Code</Link>
+        <Link to="/qr-code/scan">Scan QR Code</Link>
+        <Link to="/qr-code/generateHistory">History of Generated QR Codes</Link>
+        <Link to="/qr-code/scanHistory">History of Scanned QR Codes</Link>
     </nav>
   )
 }
